dc5409251a6835dc68176c60c2ebc53daa29a218
mallys <mik.gnu1@gmail.com>
2026-01-15 12:04:50 +0100

    vor vercel 3

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,12 @@
+
 # build_files.sh
 echo " BUILDING PROJECT START"
-python3 -m pip install -r requirements.txt
-python3 manage.py collectstatic --noinput --clear
+
+# Install dependencies
+python3.12 -m pip install -r requirements.txt
+
+# Run collectstatic. 
+# We use --noinput to prevent hangs and ensure the folder is created.
+python3.12 manage.py collectstatic --noinput
+
 echo " BUILDING PROJECT END"
