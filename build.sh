220096742b9b789eb1be7edf92e7efd44f451ae5
mallys <mik.gnu1@gmail.com>
2026-01-16 17:02:47 +0100

    Fix DB

--- build.sh
+++ build.sh
@@ -8,5 +8,7 @@ python3 -m pip install -r requirements.txt
 # Run collectstatic without database checks
 # Using --no-input and ensuring the destination exists
 python3 manage.py collectstatic --noinput --clear
+python3 manage.py migrate --noinput --clear
+python3 manage.py makemigrations --noinput --clear
 
 echo " BUILDING PROJECT END"
--- magic_sm/settings.py
+++ magic_sm/settings.py
@@ -94,7 +94,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'PASSWORD': os.getenv('DB_PASSWORD', 'if#MS@.1'),
         'HOST': os.getenv('DB_HOST', 'localhost'),
         'PORT': os.getenv('DB_PORT', '5432'),
     }
