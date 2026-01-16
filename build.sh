05657de8729cfa4ec2276ae7bf3788ab74caff00
mallys <mik.gnu1@gmail.com>
2026-01-16 17:04:53 +0100

    Fix DB  5

--- build.sh
+++ build.sh
@@ -8,7 +8,5 @@ python3 -m pip install -r requirements.txt
 # Run collectstatic without database checks
 # Using --no-input and ensuring the destination exists
 python3 manage.py collectstatic --noinput --clear
-python3 manage.py migrate --noinput --clear
-python3 manage.py makemigrations --noinput --clear
 
 echo " BUILDING PROJECT END"
